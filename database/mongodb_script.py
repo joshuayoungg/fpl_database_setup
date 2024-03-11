69a8c8734a27ea130127e0a96ed0ab8ee83f77a9
Joshua <joshuaa.youngg@gmail.com>
2024-03-11 15:57:15 -0500

    fix adding into database

--- database/mongodb_script.py
+++ database/mongodb_script.py
@@ -1,4 +1,3 @@
-# TODO: Update mongodb script to follow new api changes
 from pymongo import MongoClient
 from decouple import config
 from pymongo.server_api import ServerApi
@@ -11,10 +10,11 @@ def __get_creds():
     config.read('creds.ini')
     username = config['CREDS']['username']
     password = config['CREDS']['password']
-    return(username, password)
+    return (username, password)
 
 
 def __get_connection():
+    print('Connecting to database..')
     username, password = __get_creds()
     uri = 'mongodb+srv://{username}:{password}@epl.oysq8wl.mongodb.net/?retryWrites=true&writeConcern=majority'.format(
         username=username, password=password)
@@ -23,39 +23,28 @@ def __get_connection():
     return client
 
 
-def __add_to_mongo(client, items = None):
+def __add_to_mongo(client, items):
     db = client.epl
-    # collection = db.gameweek_stats
-    # collection.drop()
-    # collection.insert_many(items)
-    collection = db.teams
-    collection.drop()
-    collection.insert_many(items[0])
-    collection = db.players
-    collection.drop()
-    collection.insert_many(items[1])
-
-def __get_from_mongo(client):
-    db = client.epl
-    collection = db.teams
-    items = collection.find()
-    new_items = []
     for item in items:
-        new_items.append(item)
-    return new_items
+        collection = db[item[0]]
+        try:
+            collection.drop()
+            collection.insert_many(item[1])
+        except Exception as e:
+            print('Error occurred inserting into ', item[0], ', error: ', e)
+            return
+    print("Added items successfully!")
 
 
 def mongo_db_operations(operation, items=None):
+    if items is None:
+        return
     try:
         client = __get_connection()
-        if operation == "add":
-            print('Adding items...')
-            __add_to_mongo(client, items)
-            print("Added items to collection")
-        elif operation == "get":
-            print('Getting Data..')
-            items = __get_from_mongo(client)
-        client.close()
-        return items
     except Exception as e:
-        print("Error occurred: ", e)
+        print("Error occurred connecting: ", e)
+        return
+    if operation == "add":
+        print('Adding items...')
+        __add_to_mongo(client, items)
+    client.close()
